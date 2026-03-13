5787219b4b4f5375a1a15b7623b24b75123bdf8a
Zaryab Irfan <164354254+zaryabjiya@users.noreply.github.com>
2026-03-13 10:49:54 -0700

    "fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
